caf12695e5c34487d6008de2368f8b3e22d7d82c
Dwi Apriansyah <115854672+dwiapriansyah@users.noreply.github.com>
2022-11-19 09:41:10 +0700

    Add files via upload

---
+++ Variabel.java
@@ -0,0 +1,9 @@
+public class Variabel {
+	public static void main(String[] args) {
+		String Nama = "Dwi Apriansyah";
+		int Tahun	= 1998;
+		System.out.println("Assalamu'alaikum");
+		System.out.println("Nama saya : " + Nama);
+		System.out.println("Saya lahir di tahun : " + Tahun);
+	}
+}
